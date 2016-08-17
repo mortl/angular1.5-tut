49c8f72c8521a4b167bc746a55cd424f3532dfae
mortl <martin.benes66@gmail.com>
2016-08-17 17:09:48 -0400

    displaying some rules

--- content/js/app.js
+++ content/js/app.js
@@ -1,14 +1,23 @@
 var myApp = angular.module('myApp', ['ngMessages']);
 
 
-myApp.controller("TutController",['$scope','$log', TutController]);
+myApp.controller("TutController",['$scope','$filter', TutController]);
 
 
-function TutController($scope, $log, $timeout){
+function TutController($scope, $filter){
 	$scope.name = "Martin Benes";
 	$scope.job  = "Web Developer";
 
 	$scope.handle = '';
-	
+	$scope.characters = 5;
 
+	$scope.rules = [
+		{rulename: "Must be 5 characers"},
+		{rulename: "Must not contain numbers"},
+		{rulename: "Must have a captial letter"}
+	];
+	
+	$scope.lowercasehandle = function(){
+		return $filter('lowercase')($scope.handle);
+	}
 }
